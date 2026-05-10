7aac705417c252bf4f6ae94ced0616032481b015
Pongo <f8goodspoof@gmail.com>
2026-05-10 20:45:14 +0700

    Fix Vercel API request path handling

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
+$path = $_GET['path'] ?? null;
+
+if (is_string($path) && $path !== '') {
+    $path = '/' . ltrim($path, '/');
+
+    $query = $_GET;
+    unset($query['path']);
+
+    $queryString = http_build_query($query);
+
+    $_SERVER['REQUEST_URI'] = $path . ($queryString ? '?' . $queryString : '');
+    $_SERVER['PATH_INFO'] = $path;
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $_SERVER['PHP_SELF'] = '/index.php';
+}
+
 require __DIR__ . '/../public/index.php';
